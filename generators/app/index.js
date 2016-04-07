9176c5f2c16a73fcba3973c1d2e6f4adcd684f11
Jon <jon.r.mcclure@gmail.com>
2016-04-06 21:46:14 -0500

    roll 0.2.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,12 +1,8 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var mkdirp = require('mkdirp');
-var camelCase = require('camel-case');
-var fs = require('fs');
-
-// Dependency CDN files
-var optional_cdn = require('./optional_cdn.json');
-var required_cdn = require('./required_cdn.json');
+var yeoman      = require('yeoman-generator'),
+    mkdirp      = require('mkdirp'),
+    fs          = require('fs'),
+    S           = require('string');
 
 
 module.exports = yeoman.generators.Base.extend({
@@ -16,7 +12,7 @@ module.exports = yeoman.generators.Base.extend({
     this.log('    ____  __  ____   _____\n   / __ \\/  |/  / | / /   |  ____  ____  _____\n  / / / / /|_/ /  |/ / /| | / __ \\/ __ \\/ ___/\n / /_/ / /  / / /|  / ___ |/ /_/ / /_/ (__  )\n/_____/_/  /_/_/ |_/_/  |_/ .___/ .___/____/\n                         /_/   /_/            \n');
 
     var prompts = [{
-      name:'appName',
+      name:'directoryName',
       message: 'What\'s your directory name?'
     },{
       name:'awsAccessKey',
@@ -25,45 +21,6 @@ module.exports = yeoman.generators.Base.extend({
       name:'awsSecretKey',
       message: 'What\'s your AWS secret key?'
     },
-
-    /* stuff I'm adding */
-    {
-      name: 'title',
-      message: '(Metadata) What is the title of your interactive?'
-    }, {
-      name: 'description',
-      message: '(Metadata) What is the description of your interactive?'
-    }, {
-      name: 'authors',
-      message: '(Metadata) Who is/are the authors? (comma separated,no space)'
-    }, {
-      name: 'desk',
-      message: '(Metadata) What desk does this interactive belong to? (news, business, entertainment, lifestyle, opinion, sports)'
-    }, {
-      name: 'section',
-      message: '(Metadata) What section does this interactive belong to? (To see sections, go to dallasnews.com and hover over appropriate desk, sections with multiple words should be hyphen separated, ex: food-and-wine)'
-    }, {
-      name: 'keywords',
-      message: '(Metadata) What are the keywords associated with this interactive? (comma separated, no space)'
-    }, {
-      name: 'shareImage',
-      message: '(Metadata) What is the parent directory and name of the share image? (ex: images/share.jpg)'
-    }, {
-      name: 'shareImageWidth',
-      message: '(Metadata) What is the width of your share image in pixels? (standard is 1200; use just the number, no unit of measure)'
-    }, {
-      name: 'shareImageHeight',
-      message: '(Metadata) What is the height of your share image in pixels? (standard is 630; use just the number, no unit of measure)'
-    }, {
-      name: 'twitter',
-      message: '(Metadata) What is the twitter handle for the section this interactive belongs to? (@dallasnews, @sportsdaydfw, @DMNbiz, @guidelive, @DMNopinion)'
-    }, {
-      name: 'authorTwitter',
-      message: '(Metadata) What is the author\'s twitter handle? (include the @ symbol, comma separate if multiple)'
-    },
-
-
-    /* end stuff I'm adding */
     {
       type: 'checkbox',
       name: 'features',
@@ -73,12 +30,8 @@ module.exports = yeoman.generators.Base.extend({
         value: 'includeJQUI',
         checked: false
       },{
-        name: 'JQuery Swipe',
+        name: 'JQuery TouchSwipe',
         value: 'includeJQSwipe',
-        checked: false
-      },{
-        name: 'Bowser',
-        value: 'includeBowser',
         checked: false
       },{
         name: 'Modernizr',
@@ -92,11 +45,11 @@ module.exports = yeoman.generators.Base.extend({
         name: 'Leaflet.js',
         value: 'includeLeaflet',
         checked: false
-      }/*,{
+      },{
         name: 'FontAwesome',
         value: 'includeFontAwesome',
         checked: false
-      }*/,{
+      },{
         name: 'Bootstrap',
         value: 'includeBootstrap',
         checked: false
@@ -111,29 +64,17 @@ module.exports = yeoman.generators.Base.extend({
         return features && features.indexOf(feat) !== -1;
       };
 
-      this.projectName = props.appName;
-      this.appName = camelCase(props.appName);
+      this.directoryName = S(props.directoryName).slugify().s;
+      this.appName = S(props.directoryName).camelize().s;
       this.awsAccessKey = props.awsAccessKey;
       this.awsSecretKey = props.awsSecretKey;
-      this.title = props.title;
-      this.description = props.description;
-      this.authors = props.authors;
-      this.desk = props.desk;
-      this.section = props.section;
-      this.keywords = props.keywords;
-      this.shareImage = props.shareImage;
-      this.shareImageWidth = props.shareImageWidth;
-      this.shareImageHeight = props.shareImageHeight;
-      this.twitter = props.twitter;
-      this.authorTwitter = props.authorTwitter;
       this.dependencies = {};
       this.dependencies.includeJQUI = hasFeature('includeJQUI');
       this.dependencies.includeJQSwipe = hasFeature('includeJQSwipe');
-      this.dependencies.includeBowser = hasFeature('includeBowser');
       this.dependencies.includeModernizr = hasFeature('includeModernizr');
       this.dependencies.includeD3 = hasFeature('includeD3');
       this.dependencies.includeLeaflet = hasFeature('includeLeaflet');
-      /*this.dependencies.includeFontAwesome = hasFeature('includeFontAwesome');*/
+      this.dependencies.includeFontAwesome = hasFeature('includeFontAwesome');
       this.dependencies.includeBootstrap = hasFeature('includeBootstrap');
 
       done();
@@ -142,15 +83,10 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: {
     app: function () {
-      //App files
       this.fs.copyTpl(
         this.templatePath('_package.json'),
         this.destinationPath('./package.json'),
         { appName: this.appName }
-      );
-      this.fs.copy(
-        this.templatePath('_app.js'),
-        this.destinationPath('./app.js')
       );
       this.fs.copy(
         this.templatePath('_gulpfile.js'),
@@ -164,7 +100,7 @@ module.exports = yeoman.generators.Base.extend({
       /////////////////////////////////////////////////
       // Fetch remote template files from github
       // hosted in interactives_starterkit
-      
+
       // HTML
       this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/base.html','./build/templates/',function(err){});
       this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/index.html','./build/templates/',function(err){});
@@ -210,56 +146,45 @@ module.exports = yeoman.generators.Base.extend({
       ///////////////////////////////////
       // Create output directories
       mkdirp('./build/static/assets');
-      mkdirp('./preview');
-      mkdirp('./publish');
+      mkdirp('./build/misc');
+      mkdirp('./public');
     },
 
-    /*
-    // Working on this... 
-    required_dependencies: function(){
-      var dest = './build/static/vendor';
-      for(var dependency in required_cdn){
-        this.fetch(required_cdn[dependency], dest, function(err){});
+    dependencies: function(){
+      var bowerJson = {
+        name: this.appName,
+        private: true,
+        dependencies: {
+          "jquery": null
+        }
       };
-    },*/
-
-    optional_dependencies: function(){
-      // Pass a single url or an array to yeoman's fetch.
-      var dependencyFetch = function(generator, dependency){
-            var dest = './build/static/vendor';
-            if(Array.isArray(dependency)){
-              for(var i in dependency){
-                generator.fetch(dependency[i], dest, function(err){});
-              };
-            }else{
-              generator.fetch(dependency, dest, function(err){});
-            }
-          };
 
       if(this.dependencies.includeJQUI){
-        dependencyFetch(this, optional_cdn.JQUI);
+        bowerJson.dependencies["jquery-ui"] = null;
       }
       if(this.dependencies.includeJQSwipe){
-        dependencyFetch(this, optional_cdn.JQSwipe);
+        bowerJson.dependencies["jquery-touchswipe"] = null;
       }
       if(this.dependencies.includeBowser){
-        dependencyFetch(this, optional_cdn.Bowser);
-      }
-      if(this.dependencies.includeModernizr){
-        dependencyFetch(this, optional_cdn.Modernizr);
+        bowerJson.dependencies["jquery-touchswipe"] = null;
       }
       if(this.dependencies.includeD3){
-        dependencyFetch(this, optional_cdn.D3);
+        bowerJson.dependencies["d3"] = null;
       }
       if(this.dependencies.includeLeaflet){
-        dependencyFetch(this, optional_cdn.Leaflet);
+        bowerJson.dependencies["leaflet"] = null;
       }
-/*      if(this.dependencies.includeFontAwesome){
-        dependencyFetch(this, optional_cdn.FontAwesome);
-      }*/
+      if(this.dependencies.includeFontAwesome){
+        bowerJson.dependencies["fontawesome"] = null;
+      }
       if(this.dependencies.includeBootstrap){
-        dependencyFetch(this, optional_cdn.Bootstrap);
-      }    
+        bowerJson.dependencies["bootstrap"] = null;
+      }
+      this.fs.writeJSON('bower.json', bowerJson);
+      this.fs.copy(
+        this.templatePath('bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
     },
 
 
@@ -277,19 +202,18 @@ module.exports = yeoman.generators.Base.extend({
     meta: function(){
       var timestamp = new Date();
       var defaultKeywords = ["interactives","dallas","dallas news","dfw news","dallas newspaper","dallas morning news","dallas morning news newspaper"];
-      var customKeywords = this.keywords.split(",");
       var metaJson = {
-        name: this.projectName,
-        title: this.title,
+        name: this.directoryName,
+        title: "<Title>",
         publishYear: timestamp.getFullYear(),
         publishDate: timestamp.getFullYear() +"-"+(timestamp.getMonth()+1)+"-"+timestamp.getDate()+"T00:00:00Z",
-        description: this.description,
+        description: "<Description>",
         url: 'interactives.dallasnews.com/' + timestamp.getFullYear() +"/"+this.appName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()+"/",
         id: (Math.floor(Math.random() * 100000000000) + 1).toString() ,
-        authors: this.authors,
-        desk: this.desk,
-        section: this.section,
-        keywords: defaultKeywords.concat(customKeywords),
+        authors: "<Authors>",
+        desk: "<Desk>",
+        section: "<Section>",
+        keywords: defaultKeywords,
         imgURL: 'http://interactives.dallasnews.com/' + timestamp.getFullYear() +"/"+this.appName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()+"/" + this.shareImage,
         imgWidth: this.shareImageWidth,
         imgHeight: this.shareImageHeight,
@@ -302,16 +226,16 @@ module.exports = yeoman.generators.Base.extend({
     git: function () {
       this.fs.copy(
         this.templatePath('gitignore'),
-        this.destinationPath('./.gitignore'));
+        this.destinationPath('./.gitignore')
+      );
     },
   },
 
   install: function () {
     this.installDependencies({
       callback: function() {
-                // Emit a new event - dependencies installed
-                this.emit('dependenciesInstalled');
-            }.bind(this)
+        this.emit('dependenciesInstalled');
+      }.bind(this)
     });
 
     this.on('dependenciesInstalled', function() {
--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,100 +1,91 @@
 'use strict';
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-    nodemon = require('gulp-nodemon'),
     nunjucksRender = require('gulp-nunjucks-render'),
-    changed = require('gulp-changed'),
     sass = require('gulp-sass'),
     minifyCss = require('gulp-minify-css'),
+    changed = require('gulp-changed'),
     imageResize = require('gulp-image-resize'),
     imagemin = require('gulp-imagemin'),
     imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+    mainBowerFiles = require('main-bower-files'),
+    sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     zip = require('gulp-zip'),
-    gulpIgnore = require('gulp-ignore'),
     awspublish = require('gulp-awspublish'),
     confirm = require('gulp-confirm'),
     watch = require('gulp-watch'),
     batch = require('gulp-batch'),
     merge = require('merge-stream'),
-    aws = require('aws-sdk');
+    aws = require('aws-sdk'),
+    S = require('string');
 
 
 var awsJson = require('./aws.json'),
     meta = require('./meta.json'),
-    appName = meta.name.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\s+/g,'-').toLowerCase();
+    appName = S(meta.name).slugify().s;
 
 ////////////////////////////
-/// Server Tasks
+/// Server
 
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
-    script: 'app.js',
-    watch: ['app.js']
-  })
-    .on('start', function onStart() {
-      if (!called) { cb(); }
-      called = true;
-    })
-    .on('restart', function onRestart() {
-      //small reload delay
-      setTimeout(function reload() {
-        browserSync.reload({
-          stream: false
-        });
-      }, 500);
-    });
-});
-
-gulp.task('browser-sync', ['nodemon'], function () {
-  browserSync({
-    proxy: 'http://localhost:3000',
-    //Watch all files in preview directory for changes and reload
-    files: ['./preview/**/*.*'],
-    port: 4000,
-    browser: ['google-chrome']
+gulp.task('browser-sync', function () {
+  browserSync.init({
+      files: ['./public/**/*'],
+      server: {
+          baseDir: "./public/"
+      },
+      ghostMode: false
   });
 });
 
 
 ////////////////////////////
-/// Preview Tasks
+/// Public Tasks
 
 gulp.task('dependencies', function () {
 
-  var css = gulp.src('./build/static/vendor/**/*.js')
+  var js = gulp.src(mainBowerFiles('**/*.js'), { base: 'build/static/vendor' })
+    .pipe(sourcemaps.init())
     .pipe(concat('dependency-bundle.js')) // Bundle JS
     .pipe(uglify())
-    .pipe(gulp.dest('./preview/static/js'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js'));
 
-  var js = gulp.src('./build/static/vendor/**/*.css')
+  var css = gulp.src(mainBowerFiles('**/*.css'), { base: 'build/static/vendor' })
+    .pipe(sourcemaps.init())
     .pipe(concat('dependency-bundle.css')) // Bundle CSS
     .pipe(minifyCss({ keepSpecialComments : 0, processImport: false })) // Don't keep CSS comments or process import statements like google fonts
-    .pipe(gulp.dest('./preview/static/css'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/css'));
 
   return merge(css, js);
 });
 
 gulp.task('assets', function () {
   var assets = gulp.src('./build/static/assets/**/*')
-    .pipe(gulp.dest('./preview/static/assets'));
+    .pipe(gulp.dest('./public/assets'));
   return assets;
 });
 
 gulp.task('js', function () {
   // Copy data assets
   var data = gulp.src('./build/static/js/**/*.json')
-    .pipe(gulp.dest('./preview/static/js'));
+    .pipe(gulp.dest('./public/js'));
   // Copy bundled scripts
   var bundled = gulp.src('./build/static/js/**/+*.js')
+    .pipe(sourcemaps.init())
     .pipe(concat('scripts-bundle.js'))
-    .pipe(gulp.dest('./preview/static/js'));
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js'));
   // Copy non-bundled scripts
   var copied = gulp.src(['./build/static/js/**/*.js','!./build/static/js/**/+*.js'])
-    .pipe(gulp.dest('./preview/static/js'));
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js'));
 
   return merge(data, bundled, copied);
 });
@@ -133,10 +124,10 @@ gulp.task('css', ['scss','sass'] ,function () {
   // Copy bundled CSS
   var bundled = gulp.src('./build/static/css/**/+*.css')
     .pipe(concat('styles-bundle.css'))
-    .pipe(gulp.dest('./preview/static/css'));
+    .pipe(gulp.dest('./public/css'));
   // Copy non-bundled CSS
   var copied = gulp.src(['./build/static/css/**/*.css','!./build/static/css/**/+*.css'])
-    .pipe(gulp.dest('./preview/static/css'));
+    .pipe(gulp.dest('./public/css'));
 
   return merge(bundled, copied);
 });
@@ -144,19 +135,19 @@ gulp.task('css', ['scss','sass'] ,function () {
 gulp.task('png',function () {
   function resize(size){
     return gulp.src(['./build/static/images/**/*.png','!./build/static/images/**/_*.png'])
-      .pipe(changed('./preview/static/images')) // Only process changed images for speed.
+      .pipe(changed('./public/images')) // Only process changed images for speed.
       .pipe(imageResize({ width : size, upscale : false, imageMagick : true }))
       .pipe(imagemin({
-            optimizationLevel: 4, 
+            optimizationLevel: 4,
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
         }))
-      .pipe(rename(function (path)  { 
-                                      path.basename += ("-" + size.toString()); 
-                                      path.extname = path.extname.toLowerCase(); 
-                                      return path; 
+      .pipe(rename(function (path)  {
+                                      path.basename += ("-" + size.toString());
+                                      path.extname = path.extname.toLowerCase();
+                                      return path;
                                     }))
-      .pipe(gulp.dest('./preview/static/images'));
+      .pipe(gulp.dest('./public/images'));
   }
 
   // Create and copy resized pngs
@@ -172,7 +163,7 @@ gulp.task('png',function () {
 gulp.task('jpg',function () {
   function resize(size){
     return gulp.src(['./build/static/images/**/*.{jpg,JPG}','!./build/static/images/**/_*.{jpg,JPG}'])
-      .pipe(changed('./preview/static/images')) // Only process changed images for speed.
+      .pipe(changed('./public/images')) // Only process changed images for speed.
       .pipe(imageResize({ width : size, upscale : false, imageMagick : true }))
       .pipe(imageminJpegRecompress({
         loops: 3,
@@ -180,12 +171,12 @@ gulp.task('jpg',function () {
         max: 75,
         target: 0.9999
       })())
-      .pipe(rename(function (path)  { 
-                                      path.basename += ("-" + size.toString()); 
-                                      path.extname = path.extname.toLowerCase(); 
-                                      return path; 
+      .pipe(rename(function (path)  {
+                                      path.basename += ("-" + size.toString());
+                                      path.extname = path.extname.toLowerCase();
+                                      return path;
                                     }))
-      .pipe(gulp.dest('./preview/static/images'));
+      .pipe(gulp.dest('./public/images'));
   }
 
   // Create and copy resized jpgs
@@ -201,12 +192,12 @@ gulp.task('jpg',function () {
 gulp.task('img',['png','jpg'], function() {
   // Copies over SVGs or other image files
   var other = gulp.src(['./build/static/images/**/*','!./build/static/images/**/*.{png,jpg,JPG}'])
-    .pipe(changed('./preview/static/images'))
-    .pipe(gulp.dest('./preview/static/images'));
+    .pipe(changed('./public/images'))
+    .pipe(gulp.dest('./public/images'));
   // Copy explicitly non-altered imgs
   var copied = gulp.src('./build/static/images/**/_*.{png,jpg,JPG}')
-    .pipe(changed('./preview/static/images'))
-    .pipe(gulp.dest('./preview/static/images'));
+    .pipe(changed('./public/images'))
+    .pipe(gulp.dest('./public/images'));
 
   return merge(other, copied);
 });
@@ -223,7 +214,7 @@ gulp.task('templates', function () {
     nunjucksRender.nunjucks.configure(['build/templates/'], {watch: false});
     return gulp.src(['build/templates/**/*.html','!build/templates/base.html','!build/templates/partials/**/*.html'])
         .pipe(nunjucksRender(meta))
-        .pipe(gulp.dest('preview'));
+        .pipe(gulp.dest('public'));
 });
 
 
@@ -231,37 +222,10 @@ gulp.task('templates', function () {
 ////////////////////////////
 /// Publish Tasks
 
-gulp.task('mover', function(){
-  return gulp.src(['./preview/**/*','!./preview/static/**/*'])
-    .pipe(gulp.dest('./publish'));
-});
-
-gulp.task('pubStatic', function(){
-  return gulp.src(['./preview/static/**/*','!./preview/static/js/**/*.js','!./preview/static/css/**/*.css','!./preview/static/images/**/*.{png,jpg,JPG}'])
-    .pipe(gulp.dest('./publish'));
-})
-
-gulp.task('pubImg',function(){
-  return gulp.src('./preview/static/images/**/*.{png,jpg,JPG}')
-    .pipe(gulp.dest('./publish/images'));
-})
-
-gulp.task('pubJS', function(){
-  return gulp.src('./preview/static/js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./publish/js'));
-});
-
-gulp.task('pubCSS',function(){
-  return gulp.src('./preview/static/css/**/*.css')
-    .pipe(minifyCss({ keepSpecialComments : 0, processImport: false }))
-    .pipe(gulp.dest('./publish/css'));
-});
-
-gulp.task('zip', ['mover','pubStatic','pubImg','pubJS','pubCSS'], function () {
+gulp.task('zip', function () {
   return gulp.src(['./**/*','!aws.json','!node_modules/**/*','!.git/*','!./**/*.zip']) // Zip everything except aws credentials, node modules, git files and zip files
       .pipe(zip(appName + '.zip'))
-      .pipe(gulp.dest('publish'));
+      .pipe(gulp.dest('public'));
 });
 
 
@@ -269,7 +233,7 @@ gulp.task('aws', ['zip'], function() {
   var year = meta.publishYear,
       publisher = awspublish.create(awsJson);
 
-  return gulp.src('./publish/**/*')
+  return gulp.src('./public/**/*')
       .pipe(confirm({
         question: 'You\'re about to publish this project to AWS under directory \''+year+'/'+appName+'\'. In the process, we\'ll also wipe out any uploads to the test directory. Are you sure you want to do this?',
         input: '_key:y'
@@ -278,13 +242,14 @@ gulp.task('aws', ['zip'], function() {
           path.dirname = '/'+year+'/'+appName + '/' + path.dirname.replace('.\\','');
       }))
       .pipe(publisher.publish({},{force:true}))
+      .pipe(publisher.cache())
       .pipe(awspublish.reporter());
 });
 
 // Publish to a test directory.
 gulp.task('test', ['zip'], function() {
   var publisher = awspublish.create(awsJson);
-  return gulp.src('./publish/**/*')
+  return gulp.src('./public/**/*')
       .pipe(confirm({
         question: 'You\'re about to publish this project to AWS under directory test/\''+appName+'\'. Are you sure?',
         input: '_key:y'
@@ -293,6 +258,7 @@ gulp.task('test', ['zip'], function() {
           path.dirname = '/test/'+appName + '/' + path.dirname.replace('.\\','');
       }))
       .pipe(publisher.publish({},{force:true}))
+      .pipe(publisher.cache())
       .pipe(awspublish.reporter());
 });
 
@@ -301,7 +267,7 @@ gulp.task('test', ['zip'], function() {
 gulp.task('clear-test', ['aws'],function() {
 
   aws.config.update({
-        accessKeyId: awsJson.accessKeyId, 
+        accessKeyId: awsJson.accessKeyId,
         secretAccessKey: awsJson.secretAccessKey,
         region: 'us-east-1'
       });
@@ -329,7 +295,7 @@ gulp.task('clear-test', ['aws'],function() {
         return console.log(data.Deleted.length);
       });
     }
-    
+
   });
 });
 
@@ -345,10 +311,10 @@ gulp.task('default', ['assets','scss','sass','css','js','templates','dependencie
   gulp.watch('build/templates/**/*.html', ['templates']);
 });
 
-gulp.task('publish',['mover','pubStatic','pubImg','pubJS','pubCSS','zip','aws','clear-test'], function(){
-  console.log('Published at: http://interactives.dallasnews.com/'+meta.publishYear+ '/' + appName);
+gulp.task('publish',['zip','aws','clear-test'], function(){
+  console.log('Published at: http://interactives.dallasnews.com/' + meta.publishYear + '/' + appName);
 });
 
-gulp.task('publish-test',['mover','pubStatic','pubImg','pubJS','pubCSS','zip','test'],function(){
-  console.log('Preview at: http://interactives.dallasnews.com/test/' + appName);
+gulp.task('publish-test',['zip','test'],function(){
+  console.log('public at: http://interactives.dallasnews.com/test/' + appName);
 });
