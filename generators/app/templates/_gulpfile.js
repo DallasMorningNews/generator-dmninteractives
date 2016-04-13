e104b7ce2108cf81762a4e13195880e763f9875c
Jon <jon.r.mcclure@gmail.com>
2016-04-12 19:08:13 -0500

    roll 0.2.3 -- image caching and jshint

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -5,10 +5,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     minifyCss = require('gulp-minify-css'),
     changed = require('gulp-changed'),
+    del = require('del'),
     imageResize = require('gulp-image-resize'),
     imagemin = require('gulp-imagemin'),
     imageminJpegRecompress = require('imagemin-jpeg-recompress'),
     mainBowerFiles = require('main-bower-files'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
@@ -20,7 +23,9 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     batch = require('gulp-batch'),
     merge = require('merge-stream'),
-    S = require('string');
+    S = require('string'),
+    path = require('path'),
+    fs = require('fs');
 
 
 var awsJson = require('./aws.json'),
@@ -75,6 +80,8 @@ gulp.task('js', function () {
     .pipe(gulp.dest('./public/js'));
   // Copy bundled scripts
   var bundled = gulp.src('./build/static/js/**/+*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
     .pipe(sourcemaps.init())
     .pipe(concat('scripts-bundle.js'))
     .pipe(uglify())
@@ -82,6 +89,8 @@ gulp.task('js', function () {
     .pipe(gulp.dest('./public/js'));
   // Copy non-bundled scripts
   var copied = gulp.src(['./build/static/js/**/*.js','!./build/static/js/**/+*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write('./'))
@@ -132,16 +141,86 @@ gulp.task('css', ['scss','sass'] ,function () {
   return merge(bundled, copied);
 });
 
-gulp.task('png',function () {
+
+gulp.task('optimize-images', function(){
+
+  var pngs = gulp.src([
+      './build/static/images/**/*.png',
+      '!./build/static/images/opt/**/*'
+    ])
+    .pipe(changed('./build/static/images/opt'))
+    .pipe(
+        imagemin({
+          optimizationLevel: 4,
+          progressive: true,
+          svgoPlugins: [{removeViewBox: false}],
+        })
+    )
+    .pipe(gulp.dest('./build/static/images/opt'));
+
+  var jpgs = gulp.src([
+      './build/static/images/**/*.{jpg,JPG}',
+      '!./build/static/images/opt/**/*'
+    ])
+    .pipe(changed('./build/static/images/opt'))
+    .pipe(imageminJpegRecompress({
+          loops: 3,
+          min: 50,
+          max: 75,
+          target: 0.9999
+        })()
+    )
+    .pipe(gulp.dest('./build/static/images/opt'));
+
+  return merge(pngs, jpgs);
+});
+
+
+gulp.task('resize-images',['optimize-images'],function () {
   function resize(size){
-    return gulp.src(['./build/static/images/**/*.png','!./build/static/images/**/_*.png'])
-      .pipe(changed('./public/images')) // Only process changed images for speed.
+
+
+    /**
+     * From gulp-changed source:
+     * https://github.com/sindresorhus/gulp-changed/blob/master/index.js#L9
+     */
+    function fsOperationFailed(stream, sourceFile, err) {
+      if (err) {
+        if (err.code !== 'ENOENT') {
+          stream.emit('error', new gutil.PluginError('gulp-changed', err, {
+            fileName: sourceFile.path
+          }));
+        }
+        stream.push(sourceFile);
+      }
+      return err;
+    }
+
+    /**
+     * Custom comparator that takes into account filename changes.
+     */
+    function cacheCompare(stream, cb, sourceFile, targetPath) {
+      var ext = path.extname(targetPath),
+          dir = path.dirname(targetPath),
+          base = path.basename(targetPath, ext),
+          targetPath = path.join(dir, (base + ('-' + size.toString()) + ext.toLowerCase()));
+
+      fs.stat(targetPath, function (err, targetStat) {
+        if (!fsOperationFailed(stream, sourceFile, err)) {
+          if (sourceFile.stat.mtime > targetStat.mtime) {
+            stream.push(sourceFile);
+          }
+        }
+        cb();
+      });
+    }
+
+    return gulp.src([
+        './build/static/images/opt/**/*.{png,jpg,JPG}',
+        '!./build/static/images/opt/**/_*.{png,jpg,JPG}'
+      ])
+      .pipe(changed('./public/images',{hasChanged: cacheCompare}))
       .pipe(imageResize({ width : size, upscale : false, imageMagick : true }))
-      .pipe(imagemin({
-            optimizationLevel: 4,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-        }))
       .pipe(rename(function (path)  {
                                       path.basename += ('-' + size.toString());
                                       path.extname = path.extname.toLowerCase();
@@ -160,51 +239,32 @@ gulp.task('png',function () {
   return merge(s1, s2, s3, s4, s5);
 });
 
-gulp.task('jpg',function () {
-  function resize(size){
-    return gulp.src(['./build/static/images/**/*.{jpg,JPG}','!./build/static/images/**/_*.{jpg,JPG}'])
-      .pipe(changed('./public/images')) // Only process changed images for speed.
-      .pipe(imageResize({ width : size, upscale : false, imageMagick : true }))
-      .pipe(imageminJpegRecompress({
-        loops: 3,
-        min: 50,
-        max: 75,
-        target: 0.9999
-      })())
-      .pipe(rename(function (path)  {
-                                      path.basename += ('-' + size.toString());
-                                      path.extname = path.extname.toLowerCase();
-                                      return path;
-                                    }))
-      .pipe(gulp.dest('./public/images'));
-  }
 
-  // Create and copy resized jpgs
-  var s1 = resize(3000);
-  var s2 = resize(2400);
-  var s3 = resize(1800);
-  var s4 = resize(1200);
-  var s5 = resize(600);
-
-  return merge(s1, s2, s3, s4, s5);
-});
-
-gulp.task('img',['png','jpg'], function() {
+gulp.task('img',['resize-images'], function() {
   // Copies over SVGs or other image files
-  var other = gulp.src(['./build/static/images/**/*','!./build/static/images/**/*.{png,jpg,JPG}'])
+  var other = gulp.src([
+      './build/static/images/**/*',
+      '!./build/static/images/**/*.{png,jpg,JPG}',
+      '!./build/static/images/opt/**/*'
+    ],{ nodir: true })
     .pipe(changed('./public/images'))
     .pipe(gulp.dest('./public/images'));
-  // Copy explicitly non-altered imgs
-  var copied = gulp.src('./build/static/images/**/_*.{png,jpg,JPG}')
+  // Copy imgs that weren't resized
+  var copied = gulp.src('./build/static/images/opt/**/_*.{png,jpg,JPG}')
     .pipe(changed('./public/images'))
     .pipe(gulp.dest('./public/images'));
 
   return merge(other, copied);
 });
 
+
 // Watch for new images added and run img task
-gulp.task('watch', function () {
-    watch('./build/static/images/**/*', batch(function (events, done) {
+gulp.task('watch-images', function () {
+    watch([
+      './build/static/images/**/*',
+      '!./build/static/images/opt/**/*',
+      '!**/*.crdownload'
+      ], batch(function (events, done) {
         gulp.start('img', done);
     }));
 });
@@ -245,6 +305,7 @@ gulp.task('aws', ['zip'], function() {
       .pipe(publisher.cache())
       .pipe(awspublish.reporter());
 });
+
 
 // Publish to a test directory.
 gulp.task('test', ['zip'], function() {
@@ -302,7 +363,7 @@ gulp.task('clear-test', ['aws'],function() {
 ////////////////////////////
 /// Task runs
 
-gulp.task('default', ['assets','scss','sass','css','js','templates','dependencies','watch','browser-sync'], function () {
+gulp.task('default', ['assets','img','scss','sass','css','js','templates','dependencies','watch-images','browser-sync'], function () {
   gulp.watch('build/static/assets/**/*', ['assets']);
   gulp.watch('build/static/sass/**/*.{scss,sass}', ['scss','sass','css']);
   gulp.watch('build/static/js/**/*.js*', ['js']);
@@ -318,3 +379,4 @@ gulp.task('publish',['zip','aws','clear-test'], function(){
 gulp.task('publish-test',['zip','test'],function(){
   console.log('public at: http://interactives.dallasnews.com/test/' + appName);
 });
+
