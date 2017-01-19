ad988fbeb7dd2302840a7bb8990f26f1296fe577
Allan James Vestal <allanjamesvestal@gmail.com>
2017-01-18 18:29:32 -0600

    Modify 'page-browserify' sub-generator to download remote-repo files programmatically using the 'filesFromRepos' parameter, and to generate the necessary initial files for this type.

--- generators/page-browserify/index.js
+++ generators/page-browserify/index.js
@@ -1,12 +1,18 @@
 'use strict';
 
 
+var _ = require('underscore');
 var fs = require('fs');
+var github = require('octonode');
 var googleURL = require('google-url-helper');
 var mkdirp = require('mkdirp');
+var path = require('path');
 var S = require('string');
 var validURL = require('valid-url');
 var yeoman = require('yeoman-generator');
+
+
+var githubClient = github.client();
 
 
 module.exports = yeoman.Base.extend({
@@ -14,6 +20,8 @@ module.exports = yeoman.Base.extend({
     var done = this.async();
 
     this.log('Starting up an INTERACTIVE PAGE with BROWSERIFY...');
+
+    this.baseConfig = this.options.baseConfig;
 
     var prompts = [
       {
@@ -57,142 +65,152 @@ module.exports = yeoman.Base.extend({
   },
 
   writing: {
-    // app: function () {
-    //   this.fs.copyTpl(
-    //     this.templatePath('package.json'),
-    //     this.destinationPath('./package.json'),
-    //     { appName: this.appName }
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('gulpfile.js'),
-    //     this.destinationPath('./gulpfile.js')
-    //   );
-    //
-    // },
+    app: function () {
+      this.fs.copyTpl(
+        this.templatePath('package.json'),
+        this.destinationPath('./package.json'),
+        { appName: this.appName }
+      );
+      this.fs.copy(
+        this.templatePath('gulpfile.js'),
+        this.destinationPath('./gulpfile.js')
+      );
+    },
 
-    // projectfiles: function () {
-    //
-    //   /////////////////////////////////////////////////
-    //   // Fetch remote template files from github
-    //   // hosted in interactives_starterkit
-    //
-    //   // HTML
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/base.html','./build/templates/', function(err){});
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/index.html','./build/templates/', function(err){});
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/adblock1.html','./build/templates/', function(err){});
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/adblock2.html','./build/templates/', function(err){});
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/templates/adblock3.html','./build/templates/', function(err){});
-    //
-    //   // CSS & SCSS
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/css/theme.scss','./build/static/sass/', function(err){
-    //     fs.rename('./build/static/sass/theme.scss','./build/static/sass/+base.scss');
-    //   });
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/css/_mixins.scss','./build/static/sass/', function(err){});
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/css/_variables.scss','./build/static/sass/', function(err){});
-    //   // JS
-    //   this.fetch('https://raw.githubusercontent.com/DallasMorningNews/interactives_starterkit/master/js/customJS.js','./build/static/js/', function(err){
-    //     fs.rename('./build/static/js/customJS.js','./build/static/js/+custom.js');
-    //   });
-    //   ////////////////////////////////////
-    //   // Copy rest of template files
-    //
-    //   this.fs.copy(
-    //     this.templatePath('custom.scss'),
-    //     this.destinationPath('./build/static/sass/+custom.scss')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('data.json'),
-    //     this.destinationPath('./build/static/js/data.json')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('img.html'),
-    //     this.destinationPath('./build/templates/partials/img.html')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('defaultImage.jpg'),
-    //     this.destinationPath('./build/static/images/_defaultImage.jpg')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('buttonLeft.svg'),
-    //     this.destinationPath('./build/static/images/buttonLeft.svg')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('buttonRight.svg'),
-    //     this.destinationPath('./build/static/images/buttonRight.svg')
-    //   );
-    //   this.fs.copy(
-    //     this.templatePath('gitkeep'),
-    //     this.destinationPath('./build/static/vendor/.gitkeep')
-    //   );
-    //
-    //   ///////////////////////////////////
-    //   // Create output directories
-    //   mkdirp('./build/static/assets');
-    //   mkdirp('./build/static/vendor');
-    //   mkdirp('./build/misc');
-    //   mkdirp('./public');
-    // },
+    projectfiles: function () {
+      /////////////////////////////////////////////////
+      // Fetch remote template files from github
+      // hosted in interactives_starterkit
+      if (!_.isUndefined(this.baseConfig.filesFromRepos)) {
+        this.log(this.baseConfig.filesFromRepos);
+      }
 
-    // aws: function(){
-    //   var awsJson = {
-    //     accessKeyId: this.awsAccessKey,
-    //     secretAccessKey: this.awsSecretKey,
-    //     params:{
-    //         Bucket: 'interactives.dallasnews.com'
-    //       }
-    //     };
-    //   this.fs.writeJSON('aws.json', awsJson);
-    // },
+      if (!_.isUndefined(this.baseConfig.filesFromRepos)) {
+        _.each(this.baseConfig.filesFromRepos, function(repoFiles, repoName) {
+          var repo = githubClient.repo(repoName);
 
-    // meta: function(){
-    //   var timestamp = new Date();
-    //   var defaultKeywords = ['interactives','dallas','dallas news','dfw news','dallas newspaper','dallas morning news','dallas morning news newspaper'];
-    //   var metaJson = {
-    //     id: (Math.floor(Math.random() * 100000000000) + 1).toString(),
-    //     name: this.directoryName,
-    //     pageTitle: '<Title>',
-    //     shareTitle: '<Title>',
-    //     shareText: '<Text>',
-    //     tweetText: '<Text>',
-    //     publishYear: timestamp.getFullYear(),
-    //     publishDate: timestamp.getFullYear() +'-'+(timestamp.getMonth()+1)+'-'+timestamp.getDate()+'T00:00:00Z',
-    //     url: 'http://interactives.dallasnews.com/' + timestamp.getFullYear() +'/'+this.directoryName+'/',
-    //     authors: '<Authors - comma-separated, capitalize first letter of names. NOTE: If more than one author, you will need to manually edit the author key in the parsely json object. Author names should be comma separated strings within an array in the parsely json object.>',
-    //     desk: '<Desk>',
-    //     section: '<Section>',
-    //     keywords: defaultKeywords,
-    //     imgURL: 'http://interactives.dallasnews.com/' + timestamp.getFullYear() +'/'+this.appName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()+'/'+ this.shareImage,
-    //     imgWidth: '<Width - w/out "px">',
-    //     imgHeight: '<Height - w/out "px">',
-    //     sectionTwitter: '<handle - w/out "@">',
-    //     authorTwitter: '<handle - w/out "@">'
-    //   };
-    //   this.fs.writeJSON('meta.json', metaJson);
-    // },
+          _.each(repoFiles, function(file) {
+            var destFilePath = (
+              _.has(file.dest, 'name')
+            ) ? (
+              path.join(file.dest.path, file.dest.name)
+            ) : (
+              path.join(file.dest.path, path.basename(file.source))
+            );
 
-    // git: function () {
-    //   this.fs.copy(
-    //     this.templatePath('gitignore'),
-    //     this.destinationPath('./.gitignore')
-    //   );
-    // },
+            mkdirp(path.dirname(destFilePath));
+
+            repo.contents(file.source, function(error, contents) {
+              var fileContents = new Buffer(contents.content, 'base64');
+
+              fs.writeFileSync(destFilePath, fileContents);
+            });
+          });
+        });
+      }
+
+      ////////////////////////////////////
+      // Copy rest of template files
+
+      this.fs.copy(
+        this.templatePath('bundled-styles.scss'),
+        this.destinationPath('./build/static/sass/bundled-styles.scss')
+      );
+
+      this.fs.copy(
+        this.templatePath('data.json'),
+        this.destinationPath('./build/static/js/data.json')
+      );
+
+      this.fs.copy(
+        this.templatePath('img.html'),
+        this.destinationPath('./build/templates/partials/img.html')
+      );
+
+      this.fs.copy(
+        this.templatePath('defaultImage.jpg'),
+        this.destinationPath('./build/static/images/_defaultImage.jpg')
+      );
+
+      this.fs.copy(
+        this.templatePath('buttonLeft.svg'),
+        this.destinationPath('./build/static/images/buttonLeft.svg')
+      );
+
+      this.fs.copy(
+        this.templatePath('buttonRight.svg'),
+        this.destinationPath('./build/static/images/buttonRight.svg')
+      );
+
+      this.fs.copy(
+        this.templatePath('gitkeep'),
+        this.destinationPath('./build/static/vendor/.gitkeep')
+      );
+
+      ///////////////////////////////////
+      // Create output directories
+      mkdirp('./build/static/assets');
+      mkdirp('./build/static/vendor');
+      mkdirp('./build/misc');
+      mkdirp('./public');
+    },
+
+    aws: function(){
+      var awsJson = {
+        accessKeyId: this.awsAccessKey,
+        secretAccessKey: this.awsSecretKey,
+        params:{
+            Bucket: 'interactives.dallasnews.com'
+          }
+        };
+      this.fs.writeJSON('aws.json', awsJson);
+    },
+
+    meta: function(){
+      var timestamp = new Date();
+      // var defaultKeywords = ['interactives','dallas','dallas news','dfw news','dallas newspaper','dallas morning news','dallas morning news newspaper'];
+      var metaJson = {
+        id: (Math.floor(Math.random() * 100000000000) + 1).toString(),
+        name: this.directoryName,
+        // pageTitle: '<Title>',
+        // shareTitle: '<Title>',
+        // shareText: '<Text>',
+        // tweetText: '<Text>',
+        publishYear: timestamp.getFullYear(),
+        publishDate: timestamp.getFullYear() +'-'+(timestamp.getMonth()+1)+'-'+timestamp.getDate()+'T00:00:00Z',
+        url: 'http://interactives.dallasnews.com/' + timestamp.getFullYear() +'/'+this.directoryName+'/',
+        // authors: '<Authors - comma-separated, capitalize first letter of names. NOTE: If more than one author, you will need to manually edit the author key in the parsely json object. Author names should be comma separated strings within an array in the parsely json object.>',
+        // desk: '<Desk>',
+        // section: '<Section>',
+        // keywords: defaultKeywords,
+        imgURL: 'http://interactives.dallasnews.com/' + timestamp.getFullYear() +'/'+this.appName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()+'/'+ this.shareImage,
+        imgWidth: '<Width - w/out "px">',
+        imgHeight: '<Height - w/out "px">',
+        // sectionTwitter: '<handle - w/out "@">',
+        // authorTwitter: '<handle - w/out "@">'
+      };
+      this.fs.writeJSON('meta.json', metaJson);
+    },
+
+    git: function () {
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('./.gitignore')
+      );
+    },
   },
 
   install: function () {
-    // this.installDependencies({
-    //   callback: function() {
-    //     this.emit('dependenciesInstalled');
-    //   }.bind(this)
-    // });
+    this.installDependencies({
+      callback: function() {
+        this.emit('dependenciesInstalled');
+      }.bind(this)
+    });
 
-    // this.on('dependenciesInstalled', function() {
-    //     this.spawnCommand('gulp', ['img']);
-    //     this.spawnCommand('gulp');
-    // });
-  },
-
-  subgen: function () {
-      // this.composeWith('dmninteractives:gulp');
+    this.on('dependenciesInstalled', function() {
+        this.spawnCommand('gulp', ['img']);
+        this.spawnCommand('gulp');
+    });
   },
 
 });
