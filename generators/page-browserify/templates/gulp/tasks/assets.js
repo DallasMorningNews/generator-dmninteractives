da6d31d6d71a0f14dc917ae1f06b0442bcf8a432
Allan James Vestal <allanjamesvestal@gmail.com>
2017-02-03 18:05:57 -0600

    Update page-browserify assets task to reference new './src/' & './dist/' directory structure.

--- generators/page-browserify/templates/gulp/tasks/assets.js
+++ generators/page-browserify/templates/gulp/tasks/assets.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 
 
 module.exports = () =>
-    gulp.src('./build/static/assets/**/*')
-      .pipe(gulp.dest('./public/assets'));
+    gulp.src('./src/static/assets/**/*')
+      .pipe(gulp.dest('./dist/assets'));
