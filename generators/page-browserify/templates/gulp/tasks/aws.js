2b8d57c6286fb0a96b103badfa11d9e1329d3b6b
Allan James Vestal <allanjamesvestal@gmail.com>
2017-02-03 18:00:38 -0600

    Update page-browserify generator gulp commands.

--- generators/page-browserify/templates/gulp/tasks/aws.js
+++ generators/page-browserify/templates/gulp/tasks/aws.js
@@ -1,12 +1,15 @@
-const awsJson = require('./../../aws.json');
 const awspublish = require('gulp-awspublish');
 const confirm = require('gulp-confirm');
 const deline = require('deline');
 const gulp = require('gulp');
-const meta = require('./../../meta.json');
+const gutil = require('gulp-util');
 const path = require('path');
 const rename = require('gulp-rename');
 const S = require('string');
+
+
+const awsJson = require('./../../aws.json');
+const meta = require('./../../meta.json');
 
 
 const appName = S(meta.name).slugify().s;
@@ -14,19 +17,31 @@ const publisher = awspublish.create(awsJson);
 const year = meta.publishYear;
 
 
+const awsDirectory = `${year}/${appName}/`;
+
+
 module.exports = () =>
-    gulp.src('./public/**/*')
+    gulp.src('./dist/**/*')
         .pipe(confirm({
           question: deline`You're about to publish this project to AWS
-                            under the directory '${year}/${appName}'.
+                            under the directory '${awsDirectory}'.
                             In the process, we'll also wipe out any
                             uploads to the test directory.
                             Are you sure you want to do this?`,
-          input: '_key:y'
+          input: '_key:y',
         }))
-        .pipe(rename(function (path) {
-            path.dirname = `/${year}/${appName}/${path.dirname.replace('.\\','')}`;
+        .pipe(rename((filePath) => {
+          // eslint-disable-next-line no-param-reassign
+          filePath.dirname = awsDirectory + filePath.dirname.replace('.\\', '');
         }))
-        .pipe(publisher.publish({},{force:false}))
+        .pipe(publisher.publish({}, { force: false }))
         .pipe(publisher.cache())
-        .pipe(awspublish.reporter());
+        .pipe(awspublish.reporter())
+        .on(
+          'end',
+          gutil.log.bind(
+            gutil,
+            // eslint-disable-next-line comma-dangle
+            `Published at 'http://interactives.dallasnews.com/${awsDirectory}'.`
+          )  // eslint-disable-line comma-dangle
+        );
--- generators/page-browserify/templates/gulp/tasks/plain-images.js
+++ generators/page-browserify/templates/gulp/tasks/plain-images.js
@@ -6,17 +6,17 @@ const merge = require('merge-stream');
 module.exports = () => {
   // Copies over SVGs or other image files
   const other = gulp.src([
-    './build/static/images/**/*',
-    '!./build/static/images/**/*.{png,jpg,JPG}',
-    '!./build/static/images/opt/**/*'
+    './src/static/images/**/*',
+    '!./src/static/images/**/*.{png,jpg,JPG}',
+    '!./src/static/images/opt/**/*',
   ],{ nodir: true })
-    .pipe(changed('./public/images'))
-    .pipe(gulp.dest('./public/images'));
+    .pipe(changed('./dist/images'))
+    .pipe(gulp.dest('./dist/images'));
 
   // Copy imgs that weren't resized
-  const copied = gulp.src('./build/static/images/opt/**/_*.{png,jpg,JPG}')
-    .pipe(changed('./public/images'))
-    .pipe(gulp.dest('./public/images'));
+  const copied = gulp.src('./src/static/images/opt/**/_*.{png,jpg,JPG}')
+    .pipe(changed('./dist/images'))
+    .pipe(gulp.dest('./dist/images'));
 
   return merge(other, copied);
 };
