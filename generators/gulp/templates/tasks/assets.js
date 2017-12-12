068c3e81ae87f52ec34ff72e30d1cb49c43dec8c
Andrew Chavez <adchavez@gmail.com>
2017-12-12 11:34:53 -0600

    Drop now-unused gulp subgenerator

--- generators/gulp/templates/tasks/assets.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-
-module.exports = function() {
-	var assets = gulp.src('./build/static/assets/**/*')
-    	.pipe(gulp.dest('./public/assets'));
-  	return assets;
-};
